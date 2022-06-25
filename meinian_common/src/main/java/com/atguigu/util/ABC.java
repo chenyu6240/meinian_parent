b7d9eafa1dee72c3e19fe1d611ae09d06196f8b0
陈余 <694936828@qq.com>
2022-06-25 20:46:24 +0800

    新增ABC类

---
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,9 @@
+package com.atguigu.util;
+
+import java.sql.SQLOutput;
+
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+    }
+}
