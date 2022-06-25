23bdb0d05ed07eef9a50d39924554dbb0de5cdfe
陈余 <694936828@qq.com>
2022-06-25 20:52:09 +0800

    增加了dev

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("1111111111111111");
+        System.out.println("1111111111111111dev");
     }
 }
